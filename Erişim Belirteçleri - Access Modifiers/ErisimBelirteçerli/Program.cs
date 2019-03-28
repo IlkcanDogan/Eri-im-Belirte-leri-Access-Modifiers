eaaa4a143fde9d2a1b51f0688f7a4185547b28e6
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-28 23:10:58 +0300

    Add files via upload

---
+++ Erişim Belirteçleri - Access Modifiers/ErisimBelirteçerli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirteçerli
+{
+    class Program
+    {
+        /*
+         * Erişim Belirteçleri, metot ve değişkenlere olan erişimin sınırını belirtirler.
+         * 
+         * 1-Private: Sadece tanımlandığı sınıfın içinden erişilebilirler (Kalıtım ile Aktarılamaz!)
+         * 2-Public: Her yerden erişilebilir (Kalıtım ile aktarılır).
+         * 3-Internal: Sadece bulunduğu projeden erişilebilirler (Kalıtım ile aktarılır).
+         * 4-Protected: Tanımlandığı sınıfta ve o sınıfı miras alan(Kalıtım) sınıflardan 
+         * erişilebilir (Kalıtım ile aktarılır).
+         * 5-Protected Internal: Tanımlandığı sınıfın içinden ve ondan türetilen sınıfların
+         * içinden erişilebilir. Türetilen sınıfın aynı program içerisinde olmaması sorun teşkil
+         * etmez.
+         * ------------------------------------------------------------------------------------
+         * [Dikkat]:Türetilmiş bir sınıfta, temel sınıftaki elemanlardan sadece "public" ve "protected" 
+         * olarak tanımlananlar aktarılır. Erişim belirteci "private" olan elemanlar kalıtım ile
+         * aktarılamazlar!
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Erişim Belirteçleri - Access Modifiers/ErisimBelirteçerli/Siniflar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirteçerli
+{
+    class Siniflar
+    {
+        public class TemelSinif
+        {
+            protected string degisken_1 = "Temel sınıf içerisinde";
+            public string degisken_2 = "Temel sınıf içerisinde";
+            private string degisken_3 = "Temel sınıf içerisinde";
+
+        }
+
+        public class TuretilmisSinif : TemelSinif
+        {
+            public void Metot_1()
+            {
+                degisken_1 = "Türetilmiş sınıf içerisinde";
+            }
+            public void Metot_2()
+            {
+                degisken_2 = "Türetilmiş sınıf içerisinde";
+            }
+        }
+        
+    }
+    /*
+     * Erişim belirteci protected olan elemanların private elemanlardan tek farkı kalıtım yolu ile
+     * aktarılabiliyor olmasıdır. Eğer kalıtım söz konusu değilse protected elemanların private
+     * elemanlardan bir farkı kalmaz. Erişim belireci protected olan elemanları, sadece kalıtım yolu
+     * ile aktarılabilen private elemanlar olarak düşünebiliriz.
+     */
+}
